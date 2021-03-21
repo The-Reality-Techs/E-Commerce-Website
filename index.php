01d57578228cfae810c55e0a0fa1ae6f71936081
Aman Kulshreshtha <aman21102000@gmail.com>
2021-03-21 17:36:05 +0530

    image added

--- index.php
+++ index.php
@@ -7,56 +7,58 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
           crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <body>
 
-<!--Navigation Bar-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </div>
-</nav>
 
 <!--Header-->
 <header class="mainHead">
-    <div class="pink">
 
-    </div>
-    <div class="grey">
+    <!--Navigation Bar-->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light scrolling-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href=""><h4><span class="nav-link-white">FASHION</span></h4></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link " href=""><span class = "nav-link-white">Home</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href=""><span class = "nav-link-white">Shop</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><span class = "nav-link-black">Blog</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href=""><span class = "nav-link-black">About Us</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href=""><span class = "nav-link-black">Contact Us</span></a>
+                    </li>
+                </ul>
+            </div>
 
+        </div>
+    </nav>
+
+
+    <div class="pink ">
+        <img src="static/images/red.jpg" alt="" class="mainImage">
     </div>
+
+
+
 </header>
+<main>
+
+</main>
 
 
 <script src ="static/js/script.js" ></script>
