3cd6aa90ed235e1dc968af4c6bcf4febe4a53a3e
Aman Kulshreshtha <52315847+Aman-kulshreshtha@users.noreply.github.com>
2021-07-07 12:18:59 +0530

    Update db_connect.php
    remote sql connection

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,15 @@
 <?php
 
-    $server = "localhost:3307";
-    $username = "root";
-    $password="";
-    $database="e-commerce";
+    //development connnection
+    //$server = "localhost:3307";
+    //$username = "root";
+    //$password="";
+    //$database="e-commerce";
+
+   $server = "remotemysql.com";
+    $username = "mjwMDeVcd7";
+    $password="hghfoE8chq";
+    $database="mjwMDeVcd7";
 
     $conn = mysqli_connect($server,$username,$password,$database);
     
